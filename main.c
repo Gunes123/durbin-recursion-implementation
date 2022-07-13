4039d7079fd61aa6a95265541f2e1e4c07ee2bc1
Güneş Demirci <51853908+Gunes123@users.noreply.github.com>
2022-07-13 03:06:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 #include "stat.h"
 
 int main() {
-    int i;
     float result_corr_coeffs[FRAME_SIZE];
     float result_lpc_coeffs[FRAME_SIZE];
     float past_coeffs[FRAME_SIZE];
+    FILE *fp;
+    fp=fopen("LPCResult.txt","w+");
     auto_corr_arr(input_arr,result_corr_coeffs,201,FRAME_SIZE);
     ldr_lpc_coeff(result_corr_coeffs,result_lpc_coeffs,past_coeffs, 201 );
-    for(i=0 ; i < FRAME_SIZE; i++ )
-        printf("%f",result_lpc_coeffs[i]);
+    for(int i = 0;i<FRAME_SIZE;i++) {
+        fprintf(fp, "%f\n", result_lpc_coeffs[i]);
+    }
+    fclose(fp);
     return 0;
 }
-
-
 
 
